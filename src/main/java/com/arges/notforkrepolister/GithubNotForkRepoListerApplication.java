11ecfcc1dcdca055fd68fe604a4768d78c95db93
arges8 <marcinosiak58@gmail.com>
2024-08-04 13:44:14 +0200

    Initial commit

---
+++ src/main/java/com/arges/notforkrepolister/GithubNotForkRepoListerApplication.java
@@ -0,0 +1,13 @@
+package com.arges.notforkrepolister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubNotForkRepoListerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubNotForkRepoListerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arges/notforkrepolister/GithubNotForkRepoListerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arges.notforkrepolister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubNotForkRepoListerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
